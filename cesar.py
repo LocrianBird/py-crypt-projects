2120e55d2713d0a9b5939407af7c519a1d03fe0d
Maggie Podliian <mkanareika@gmail.com>
2021-04-24 22:01:49 +0200

    cypher edited styles

--- cesar.py
+++ cesar.py
@@ -1,5 +1,7 @@
-arr1=['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-      'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+arr1 = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+        'W', 'X', 'Y', 'Z',
+        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+        'w', 'x', 'y', 'z']
 
 arr2 = []
 for i in range(len(arr1)):
@@ -7,9 +9,9 @@ for i in range(len(arr1)):
 
 
 def change_arr2():
-  for i in range(number):
-    arr2.append(arr2[0])
-    arr2.remove(arr2[0])
+    for i in range(number):
+        arr2.append(arr2[0])
+        arr2.remove(arr2[0])
 
 
 print("1) Crypt the text/file")
@@ -18,68 +20,68 @@ print("3) Decrypt text from the terminal\n")
 
 try:
 
-  ans = int(input("[*] Write the number:\n[number] > "))
+    ans = int(input("[*] Write the number:\n[number] > "))
 
-  if ans == 1:
-    number=int(input("[*] Write the key-number [0-%s]: "%(str(len(arr1)))))
+    if ans == 1:
+        number = int(input("[*] Write the key-number [0-%s]: " % (str(len(arr1)))))
 
-    change_arr2()
+        change_arr2()
 
-    msg = input("\n[*] Write the text:\n[text] >> ")
+        msg = input("\n[*] Write the text:\n[text] >> ")
 
-    msgc = ""
-    for i in msg:
-      for j in range(len(arr1)):
-        if i == arr1[j]:
-          msgc += arr2[j]
-    crypt=open("text_crypt.txt","w")
-    print("\nCrypt-text: "+msgc+"\n")
-    crypt.write(msgc)
-    crypt.close()
+        msgc = ""
+        for i in msg:
+            for j in range(len(arr1)):
+                if i == arr1[j]:
+                    msgc += arr2[j]
+        crypt = open("text_crypt.txt", "w")
+        print("\nCrypt-text: " + msgc + "\n")
+        crypt.write(msgc)
+        crypt.close()
 
-    print(arr1)
-    print()
-    print(arr2)
+        print(arr1)
+        print()
+        print(arr2)
 
-  elif ans == 2:
-    number = int(input("[*] Write the key-number [0-%s]: "%(str(len(arr1)))))
+    elif ans == 2:
+        number = int(input("[*] Write the key-number [0-%s]: " % (str(len(arr1)))))
 
-    change_arr2()
+        change_arr2()
 
-    decrypt_r = open("text_crypt.txt","r")
-    read = decrypt_r.read()
-    msgd = ""
-    for i in read:
-      for j in range(len(arr1)):
-        if i == arr2[j]:
-          msgd += arr1[j]
-    print("\n[*] Decrypted text:")
-    print("[text] << "+str(msgd))
-    answer = input("\n[*] Save decrypted text in the file?\n[y/n] > ")
-    if answer == "y" or answer == "Y":
-      decrypt_w=open("text_decrypt.txt","w")
-      decrypt_w.write(msgd)
-      decrypt_w.close()
-      print("\n[+] File 'text_decrypt.txt' successfully saved! ")
+        decrypt_r = open("text_crypt.txt", "r")
+        read = decrypt_r.read()
+        msgd = ""
+        for i in read:
+            for j in range(len(arr1)):
+                if i == arr2[j]:
+                    msgd += arr1[j]
+        print("\n[*] Decrypted text:")
+        print("[text] << " + str(msgd))
+        answer = input("\n[*] Save decrypted text in the file?\n[y/n] > ")
+        if answer == "y" or answer == "Y":
+            decrypt_w = open("text_decrypt.txt", "w")
+            decrypt_w.write(msgd)
+            decrypt_w.close()
+            print("\n[+] File 'text_decrypt.txt' successfully saved! ")
+        else:
+            pass
+        decrypt_r.close()
+
+    elif ans == 3:
+        number = int(input("[*] Write the key-number [0-%s]: " % (str(len(arr1)))))
+
+        change_arr2()
+
+        msg = input("\n[*] Write the crypted text:\n[text] >> ")
+        msgd = ""
+        for i in msg:
+            for j in range(len(arr1)):
+                if i == arr2[j]:
+                    msgd += arr1[j]
+        print("\n[*] Decrypted text:")
+        print("[text] << " + str(msgd))
     else:
-      pass
-    decrypt_r.close()
-
-  elif ans == 3:
-    number = int(input("[*] Write the key-number [0-%s]: "%(str(len(arr1)))))
-
-    change_arr2()
-
-    msg = input("\n[*] Write the crypted text:\n[text] >> ")
-    msgd = ""
-    for i in msg:
-      for j in range(len(arr1)):
-        if i == arr2[j]:
-          msgd += arr1[j]
-    print("\n[*] Decrypted text:")
-    print("[text] << "+str(msgd))
-  else:
-    print("Number is not defined!")
+        print("Number is not defined!")
 
 except ValueError:
-  print("Error! Print only Integer numbers!")
+    print("Error! Print only Integer numbers!")
